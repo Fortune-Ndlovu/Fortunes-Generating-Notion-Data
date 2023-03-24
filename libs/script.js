de97088bcd17efdb3e593f78221a5792eb10222b
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-03-24 15:00:56 +0000

    Initial Prototype

---
+++ libs/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".btn").addEventListener("click", () => {
+  window.open(
+    "https://purrfect-truffle-de8.notion.site/Interaction-Design-ef801e7905314ce7ad3b032abc49a48e",
+    "_blank"
+  );
+});
